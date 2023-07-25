c7e88ac32b694edea2aa6fab2fae6a3c306f14ac
ramiro <ramigarcia10@gmail.com>
2023-07-24 21:17:53 -0300

    acomodo

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-//Variables
+    //Variables
 let precio;
 let sumaTotal = 0;
 let salir = false;
 let i = 0;
 let nombre;
-//proceso
+    //proceso
 nombre= prompt("Ingrese su nombre");
 sumaDeComparas();
 alert("Hola "+ nombre +" la suma total es: " + sumaTotal);
-
-//Funcion de suma de compras
+    //Funcion de suma de compras
 function sumaDeComparas(){
     while(salir != true){
         precio= parseFloat(prompt("Ingrese el valor del producto " + i));
